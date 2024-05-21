9acd158e7cedf95b3ca349ca6cdadf827bf6ffa9
vanpangea <vanessa.villa@pangea.cloud>
2024-05-21 00:29:30 -0700

    a

--- components/Hangar.tsx
+++ components/Hangar.tsx
@@ -26,7 +26,7 @@ export function Hangar() {
     <main className="flex flex-col h-screen w-full bg-[#1e1e1e] text-white">
       <div>
         <div className="relative">
-          <img alt="Banner" className="object-cover" fill src="hangar.gif" />
+          <img alt="Banner" className="object-cover" src="hangar.gif" />
         </div>
       </div>
       <div className="flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-8">
